8bfc609c3f236a29e10c765a5aefe42a46a98468
Jodie Amelia Watson <jodie.watson@ferchau.com>
2023-01-24 13:39:09 +0100

    Basic Project Structure

---
+++ Wpf/Navigation.cs
@@ -0,0 +1,43 @@
+﻿namespace Brainstorm.Mvvm
+{
+    public class Navigation : IDisposable
+    {
+        private Navigation()
+        {
+
+        }
+        public static void Start(string[] viewNamespaces, string[] viewModelNamespaces, string[] serviceNamespaces )
+        {
+            if (Instance == null)
+            {
+                Instance = new Navigation();
+            }
+        }
+
+        public static void Stop()
+        {
+            if (Instance != null)
+            {
+                Instance.Dispose();
+                Instance = null;
+            }
+        }
+        
+        public static Navigation Instance { get; set; }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        ~Navigation()
+        {
+            Dispose(false);
+        }
+
+        protected virtual void Dispose(bool isDisposing)
+        {
+
+        }
+
+    }
+}
